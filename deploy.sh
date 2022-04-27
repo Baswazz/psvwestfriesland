2c090a6ed3223fe6fb94893ca97c5cfcd891f576
Bas Kardinaal <bkardinaal@trumanscout.com>
2022-04-27 14:02:22 +0200

    Script update

--- deploy.sh
+++ deploy.sh
@@ -18,11 +18,6 @@ touch .nojekyll
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Baswazz/rvwestfriesland.git main:gh-pages
+git push -f git@github.com:Baswazz/psvwestfriesland.git main:gh-pages
 
 cd -
