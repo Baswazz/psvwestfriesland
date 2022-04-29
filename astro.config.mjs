9b759c47c9b7647d2763a548359a79961bd62688
Bas Kardinaal <bkardinaal@trumanscout.com>
2022-04-29 17:16:50 +0200

    url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://psvwestfriesland.nl',
-  base: 'psvwestfriesland'
+  site: 'https://psvwestfriesland.nl'
 });
